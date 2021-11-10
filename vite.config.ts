db2436582dd77898e07b57aff69496c96decc45e
Johann Schopplich <mail@johannschopplich.com>
2021-11-10 09:17:22 +0100

    refactor: inline postcss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
 import { resolve } from "path";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { defineConfig } from "vite";
 import FullReload from "vite-plugin-full-reload";
+import type { Plugin as PostCssPlugin } from "postcss";
+
+/**
+ * Prevent FOUC in development mode before Vite
+ * injects the CSS into the page
+ */
+const postCssWriteFile = (): PostCssPlugin => ({
+  postcssPlugin: "postcss-write-file",
+
+  Root(root, postcss) {
+    // @ts-expect-error: property unknown
+    if (postcss.result.opts.env === "production") return;
+
+    const distDir = resolve(__dirname, "public/assets/dev");
+    if (!existsSync(distDir)) mkdirSync(distDir);
+    writeFileSync(`${distDir}/index.css`, root.source?.input.css ?? "");
+  },
+});
 
 export default defineConfig(({ mode }) => ({
   root: "src",
@@ -15,8 +34,16 @@ export default defineConfig(({ mode }) => ({
     },
   },
 
+  css: {
+    postcss: {
+      plugins: [postCssWriteFile()],
+    },
+  },
+
   plugins: [
-    FullReload(["site/{snippets,templates}/**/*"], { root: __dirname }),
+    FullReload(["site/snippets/**/*", "site/templates/**/*"], {
+      root: __dirname,
+    }),
   ],
 
   server: {
