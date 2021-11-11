1472b2181ba52ec05af3bafae9fdd56e3cf0f1c4
Johann Schopplich <mail@johannschopplich.com>
2021-11-11 10:23:59 +0100

    chore: upgrade unocss-cli

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('@unocss/vite').UnocssPluginOptions} */
-module.exports = {
+import { defineConfig } from "unocss";
+
+export default defineConfig({
   shortcuts: [
     { box: "max-w-7xl mx-auto bg-gray-100 rounded-md shadow-sm p-4" },
   ],
-};
+});
