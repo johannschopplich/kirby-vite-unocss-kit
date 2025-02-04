f61ceca485d36b0c85dbd75d6d4eec26c903f730
Johann Schopplich <mail@johannschopplich.com>
2025-02-04 18:33:39 +0100

    fix: update image source method

--- site/snippets/blocks/image.php
+++ site/snippets/blocks/image.php
@@ -16,7 +16,7 @@ if ($block->location() === 'web') {
   if ($caption->isEmpty()) $caption = $image->caption();
 
   $img = Html::img(
-    $image->src(),
+    $image->url(),
     [
       'loading' => 'lazy',
       'data-srcset' => $image->srcset(),
